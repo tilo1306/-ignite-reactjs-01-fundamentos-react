74b19c90d1a7d0166d0edd328ce66de34ec6634c
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-04-14 15:54:12 -0300

    feat: Estilizações do Post

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { Header } from './components/Header'
 import './global.css'
 import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 function App() {
 
   return (
     <div>
       <Header />
-      <div className={styles.wrapper}></div>
-      <Sidebar />
-      <main></main>
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+        <Post />
+        <main></main>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
-import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
@@ -8,17 +9,20 @@ export function Sidebar() {
                 className={styles.cover}
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
+
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/tilo1306.png" />
-                <strong>Douglas S. Oliveira</strong>
+                <Avatar src="https://github.com/tilo1306.png" />
+
+                <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
+
             <footer>
                 <a href="#">
                     <PencilLine size={20} />
-                    Editar seu Perfil
+                    Editar seu perfil
                 </a>
             </footer>
         </aside>
-    )
+    );
 }
